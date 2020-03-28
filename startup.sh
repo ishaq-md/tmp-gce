030df1d456f06bbe909b951726392e5a00aff980
ishaqmdgcp <55760349+ishaqmdgcp@users.noreply.github.com>
2020-03-28 17:00:31 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@ sudo su
 apt-get update
 apt-get install git -y 
 apt-get install unzip -y
-wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip 
-unzip terraform_0.12.0_linux_amd64.zip 
-sudo mv terraform /usr/local/bin/ 
-terraform --version 
+#wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip 
+#unzip terraform_0.12.0_linux_amd64.zip 
+#sudo mv terraform /usr/local/bin/ 
+#terraform --version 
 gcloud init
 #git clone https://github.com/ishaqmdgcp/gce-startup.git
 #cd /gce-startup 
